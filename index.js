b9caeb614abb23d3e13b7e7d3e56a3895f4cffb5
Master <varunaggarwal102@gmail.com>
2024-03-19 23:27:11 +0530

    First commit : create the directory and setup express server

---
+++ index.js
@@ -0,0 +1,11 @@
+ const express=require('express');
+ const app=express();
+ const port=8000;
+
+app.listen(port,function(err){
+if(err){
+    console.log(`there is an error : ${err}`)
+}
+
+console.log(`server is running on port: ${port}`)
+})
